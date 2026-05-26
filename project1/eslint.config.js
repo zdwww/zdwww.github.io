da382d037fd8d9477783682f552229c0b7a77c4b
James Hicks <jchicks2022@gmail.com>
2026-05-26 18:49:29 -0400

    update eslint config
    Updates eslint config to class eslint found in Slack.
    
    Hi! Daiwei! I really enjoyed browsing your personal website, reading about some of your academic contributions, as well as your personal page. The layout and design of the personal page especially was pleasing and cohesive. The font choices were also great. I really enjoyed your Cabinet of Cats and learning about each of their distinct personalities. Everything feels very polished, and the ability to sort them using JS was a nice touch. 
    
    One thing to note, is to make sure that the project-1 folder uses the class ESLint config that can be found in Slack. It looks like you aligned in most other areas, including ES6 modules, in terms of format. The ESLint config did take me some time to find myself.

--- project1/eslint.config.js
+++ project1/eslint.config.js
@@ -1,26 +1,58 @@
-// ESLint v9+ flat config.
-// A legacy .eslintrc.json is also kept for graders running ESLint v8.
+import globals from 'globals';
+import js from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import prettier from 'eslint-plugin-prettier';
 
 export default [
   {
-    files: ['js/**/*.js'],
+    files: ['**/*.{js,jsx,mjs,cjs,ts,tsx}'],
+
     languageOptions: {
       ecmaVersion: 'latest',
       sourceType: 'module',
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+
       globals: {
-        window: 'readonly',
-        document: 'readonly',
-        Image: 'readonly',
-        fetch: 'readonly',
-        console: 'readonly',
-        requestAnimationFrame: 'readonly',
-        cancelAnimationFrame: 'readonly',
-        CustomEvent: 'readonly'
-      }
+        ...globals.browser,
+        ...globals.node,
+        ...globals.es2025,
+      },
     },
+    plugins: {
+      prettier: prettier,
+    },
+
     rules: {
-      'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
-      'no-console': 'off'
-    }
-  }
+      // ESLint recommended rules
+      ...js.configs.recommended.rules,
+
+      indent: [
+        'error',
+        2,
+        {
+          SwitchCase: 1,
+        },
+      ],
+
+      'linebreak-style': ['error', 'unix'],
+      quotes: ['error', 'double'],
+      semi: ['error', 'always'],
+      'no-console': 0,
+
+      // Prettier integration - this runs Prettier through ESLint
+      'prettier/prettier': [
+        'error',
+        {
+          endOfLine: 'lf',
+          trailingComma: 'es5',
+          singleQuote: false,
+        },
+      ],
+    },
+  },
+  eslintConfigPrettier,
 ];
